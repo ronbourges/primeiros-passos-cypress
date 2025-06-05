953bca441e9c5d268b30188d10381601a6caa94a
ronbourges <bourgesron@mail.com>
2025-06-04 23:36:46 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM Tests', () => {
+  it('Login - success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']") .type( 'admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+  it('Login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type( 'Test')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert')
+    
+  })
+})
